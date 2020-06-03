05c8a1ddc26ebf1d10c9158a4cc531e6c707c253
Bo Tranberg <bo@tberg.dk>
2020-06-03 11:28:06 +0200

    Add AJAX test

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 import json
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def index():
-    d = pd.DataFrame(data={'a':[1, 2, 3], 'b': [4, 5 ,6]})
+    d = pd.DataFrame(data={'a': [1, 2, 3], 'b': [4, 5, 6]})
     return render_template('index.html', data=json.loads(d.to_json(orient='records')))
 
-@app.route("/test")
+
+@app.route('/test')
 def test():
     return 'Success!'
+
+
+@app.route('/async_test', methods=['POST'])
+def async_test():
+    return json.dumps({'status': 'OK', 'test': request.form['test']})
 
 
 if __name__ == '__main__':
